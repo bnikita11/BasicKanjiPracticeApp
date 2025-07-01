19a26a0df8595d002bfbd8aeb6d00fbf5e317546
Nikita Baniya <nikitabaniya57@gmail.com>
2025-07-01 16:12:20 +0545

    Force Vercel to serve static files correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    publicDir: 'public',  // This ensures that Vercel picks up the 'public' directory
+
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
